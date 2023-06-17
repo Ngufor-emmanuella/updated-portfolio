d4afa6b6de12e6071c1476aff4c92bc85852d6ce
Ngufor emma <98039632+Ngufor-emmanuella@users.noreply.github.com>
2023-06-17 22:29:46 +0100

    Add icons

---
+++ modal.js
@@ -0,0 +1,177 @@
+async function displayProjects() {
+  const div = document.getElementById("projectAera");
+  div.innerHTML = `
+  <section>
+  <div  id="second-sec">
+  <div id ="my-recnt">
+  <h3 class="second-tittle">
+    My Recent Works
+  </h3>
+  <hr class="line2">
+</div>
+<div id="multi">
+ <div>
+  <img class="img-desk" width="327" height="237" src="./images/air-air.png" alt="Budget-app">
+  </div>
+  <div>
+  <img class="img-desk2" width="327" height="237" src="./images/air-air.png" alt="Budget-app">
+  </div>
+  <div class="post">
+    <h3 class="second-subtittle">
+      Multi-Post Stories
+    </h3>
+  <div id="second-p">
+    <p class="second-p">
+    In this project, I used React-Redux and API to build SPA a website displaying all African countries that are polluted, and when the user clicks on each country or searches for a country its displays the various cities. This app offers an easy way to access the latest polluted country 
+    in Africa and search by counry name, making it a valuable resource for staying informed about Africa.
+    </p>
+  </div>
+  <div onclick="appear()">
+    <ul class="list-btn">
+      <li class="css-btn">HTML</li>
+      <li class="html-btn">CSS</li>
+      <li class="boot-btn">React</li>
+      <li class="ruby-btn">Redux</li>
+    </ul>
+    <div class="projectBtn">
+    <button type="button" class="btn-top" onclick="appear()" type="button"">
+    <strong>See projests</strong>
+    <div id="container-stars">
+      <div id="stars"></div>
+    </div>
+  
+    <div id="glow">
+      <div class="circle"></div>
+      <div class="circle"></div>
+    </div>
+  </button>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+</section>`;
+}
+
+displayProjects();
+
+const modalInformation = {
+  heading1: "Multi Post Stories",
+  title2: "stay updated on the latest Polluted statistics",
+  description:
+  "In this project, I used React-Redux and API to build SPA a website displaying all African countries that are polluted, and when the user clicks on each country or searches for a country its displays the various cities.",
+  description2:
+    "This app offers an easy way to access the latest polluted country in Africa and search by counry name, making it a valuable resource for staying informed about africa.",
+  imageSrc: "",
+  button1:
+    'See Live  <a href ="https://resonant-raindrop-6d1bf0.netlify.app/"><img src="./images/air-air" alt="polluion-ap"></a>',
+  button2:
+    'See Source <a href ="https://resonant-raindrop-6d1bf0.netlify.app/"><img src="./images/Vectorhub.png"alt="Budget-ap"></a>',
+  li: "html",
+  li2: "Bootstrap",
+  li3: "Ruby on rails",
+};
+
+const div1 = document.createElement("div");
+div1.className = "modal";
+document.body.appendChild(div1);
+
+const div = document.createElement("div");
+div.className = "modal-header";
+div1.appendChild(div);
+
+const div2 = document.createElement("div");
+div2.className = "title";
+div.appendChild(div2);
+
+const h2 = document.createElement("h1");
+h2.className = "modal-headm";
+h2.textContent = modalInformation.heading1;
+div2.appendChild(h2);
+
+const h20 = document.createElement("div");
+h20.className = "modal-header22";
+div2.appendChild(h20);
+
+const h22 = document.createElement("h1");
+h22.className = "modal-headd";
+h22.textContent = modalInformation.title2;
+h20.appendChild(h22);
+
+const span = document.createElement("button");
+span.className = "close-button";
+span.innerHTML = "&times;";
+div.appendChild(span);
+
+const div3 = document.createElement("div");
+div3.className = "modal-tags";
+div1.appendChild(div3);
+
+const ul = document.createElement("ul");
+ul.className = "modal-social";
+div3.appendChild(ul);
+
+const li = document.createElement("li");
+li.className = "tags1";
+li.innerHTML = modalInformation.li;
+ul.appendChild(li);
+
+const li2 = document.createElement("li");
+li2.className = "tags2";
+li2.innerHTML = modalInformation.li2;
+ul.appendChild(li2);
+
+const li3 = document.createElement("li");
+li3.className = "tags3";
+li3.innerHTML = modalInformation.li3;
+ul.appendChild(li3);
+
+const div4 = document.createElement("div");
+div4.className = "modal-pic";
+div1.appendChild(div4);
+
+const div12 = document.createElement("div");
+div12.className = "modal-body";
+div4.appendChild(div12);
+
+const p = document.createElement("p");
+p.className = "modal-mobile";
+p.textContent = modalInformation.description2;
+div12.appendChild(p);
+
+const p2 = document.createElement("p");
+p2.className = "modal-desktop";
+p2.textContent = modalInformation.description;
+div12.appendChild(p2);
+
+const div13 = document.createElement("div");
+div13.className = "modal-button";
+div1.appendChild(div13);
+
+const button = document.createElement("button");
+button.className = "live-button";
+button.innerHTML = modalInformation.button1;
+div13.appendChild(button);
+
+const button2 = document.createElement("button");
+button2.className = "source-button";
+button2.innerHTML = modalInformation.button2;
+div13.appendChild(button2);
+
+const parentModal = document.querySelector(".modal");
+const openModalButtons = document.querySelector("pro-btn");
+const closeModalButtons = document.querySelector(".close-button");
+
+function disappear() {
+  parentModal.style.display = "none";
+}
+
+closeModalButtons.addEventListener("click", disappear);
+
+function appear() {
+  parentModal.style.display = "block";
+}
+
+// appear();
+
+openModalButtons.addEventListener("onclick", appear);
---
+++ script.js
@@ -0,0 +1,100 @@
+// Hambuger menu
+const hamburger = document.querySelector('.ambuger');
+const navItem = document.querySelector('.nav-item');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navItem.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navItem.classList.remove('active');
+}));
+
+
+// Local storag
+const formName = document.getElementById('name1');
+const email = document.getElementById('text1');
+const formText = document.getElementById('msg');
+
+function autoFill() {
+  const storageObj = {
+    name: localStorage.setItem('customerName', formName.value),
+    email: localStorage.setItem('customerEmail', email.value),
+    customerInfo: localStorage.setItem('customerInfo', formText.value),
+  };
+  return storageObj;
+}
+
+formName.value = localStorage.getItem('customerName');
+email.value = localStorage.getItem('customerEmail');
+formText.value = localStorage.getItem('customerInfo');
+
+autoFill();
+
+formName.onchange = autoFill;
+email.onchange = autoFill;
+formText.onchange = autoFill;
+
+// Email and full Form validation
+function showMessage(input, message, type) {
+  const msg = input.parentNode.querySelector('small');
+  msg.style.visibility = 'visible';
+  setTimeout(() => {
+    msg.style.visibility = 'hidden';
+  }, 3000);
+  msg.innerText = message;
+  input.className = type ? '' : 'input-error';
+  return type;
+}
+
+function showError(input, message) {
+  return showMessage(input, message, false);
+}
+
+function showSuccess(input) {
+  return showMessage(input, '', true);
+}
+
+function hasValue(input, message) {
+  if (input.value.trim() === '') {
+    return showError(input, message);
+  }
+  return showSuccess(input);
+}
+
+function validateEmail(input, requiredMsg, invalidMsg) {
+  // check value
+  if (!hasValue(input, requiredMsg)) {
+    return false;
+  }
+
+  // Regegura
+
+  const emailRegex = /^[a-z][a-z0-9_@.]*$/;
+
+  const email = input.value.trim();
+  if (!emailRegex.test(email)) {
+    return showError(input, invalidMsg);
+  }
+  return true;
+}
+
+const form = document.querySelector('#contact-from-validation');
+
+const NAME_REQUIRED = 'Please enter your name';
+const EMAIL_REQUIRED = 'Please enter your email';
+const EMAIL_INVALID = 'Your email address should be in lowercase';
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const nameValid = hasValue(form.elements.name, NAME_REQUIRED);
+  const emailValid = validateEmail(form.elements.email, EMAIL_REQUIRED, EMAIL_INVALID);
+  if (nameValid && emailValid) {
+    form.submit();
+    form.elements.name.value = '';
+    form.elements.email.value = '';
+    form.elements.message.value = '';
+  }
+});
