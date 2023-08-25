3c29f7d51d1753d3d29038cf27ee5dd3cb00d0d6
Ngufor emma <98039632+Ngufor-emmanuella@users.noreply.github.com>
2023-08-25 04:07:21 +0100

    Add source link

--- project.js
+++ project.js
@@ -4,7 +4,8 @@ const projects = [
       name: 'To-Do list Applicaion',
       description: 'Through the website, users can store data locally and perform editing, updating, and deletion of the stored data directly from the webpage.',
       featuredImageBig: 'images/to-do-list.jpg',
-      technologies: ['html', 'CSS', 'JavaScripts'],
+      technologies: ['html', 'CSS', 'JavaScripts',],
+      button2: 'https://github.com/Ngufor-emmanuella/To-do-list.git><img src="./images/Vectorhub.png"alt="Budget-ap">',
       url: 'https://flourishing-begonia-645284.netlify.app/',
     },
     {
@@ -13,6 +14,7 @@ const projects = [
       description: 'The Math App project is designed as a single-page application, enabling users to perform various mathematical calculations effortlessly.',
       featuredImage: 'images/math-magic.png',
       technologies: ['html', 'CSS', 'React.js'],
+      button2: 'https://github.com/Ngufor-emmanuella/math-magicians.git><img src="./images/Vectorhub.png"alt="Budget-ap">',
       url: 'https://capable-kelpie-051989.netlify.app/',
     },
     {
@@ -21,6 +23,7 @@ const projects = [
       description: 'This a conference site that promotes healthy lifestyle and better food choices amongst all age groups.built with HTML, CSS',
       featuredImage: 'images/food-proj.png',
       technologies: ['html', 'CSS', 'JavaScripts'],
+      button2: 'https://github.com/Ngufor-emmanuella/Microverse-capstone><img src="./images/Vectorhub.png"alt="Budget-ap">',
       url: 'https://peppy-pastelito-765b3e.netlify.app/',
     }, 
     {
@@ -29,6 +32,7 @@ const projects = [
       description: 'Task creation, reading, updating, and deletion are available to users, as well as the ability to view and delete items.',
       featuredImage: 'images/shopping.png',
       technologies: ['html','AngularJS', 'JavaScripts'],
+      button2: '(https://github.com/Ngufor-emmanuella/test-project.git><img src="./images/Vectorhub.png"alt="Budget-ap">',
       url: 'https://superlative-tarsier-283b51.netlify.app/',
     },
     {
@@ -37,6 +41,7 @@ const projects = [
       description: 'This is a Full-stack application.it displays cars where the user can log in or create an account, sign in, and select cars whose items are being saved in the database',
       featuredImage: 'images/car-app.png',
       technologies: ['html', 'React', 'React-Rails'],
+      button2: 'https://github.com/Ngufor-emmanuella/full-stack-api.git><img src="./images/Vectorhub.png"alt="Budget-ap">',
       url: 'https://deploy-preview-20--jocular-kheer-33ba60.netlify.app/',
     },
     {
@@ -45,6 +50,7 @@ const projects = [
       description: 'a web app thats used to store different components of the app and also display rockets when put into use..',
       featuredImage: 'images/space-pic.png',
       technologies: ['html', 'CSS', 'JavaScript'],
+      button2: 'https://github.com/Ngufor-emmanuella/space-traveler-hub><img src="./images/Vectorhub.png"alt="Budget-ap">',
       url: 'https://splendid-kulfi-220214.netlify.app/',
     },
   ];
@@ -88,6 +94,21 @@ const projects = [
     btnSeeProject.appendChild(aLink);
   
     divProject.appendChild(btnSeeProject);
+
+
+     // creating variables for source codes
+     const btnSeeLive = document.createElement('button');
+     const btnLive = document.createTextNode('See Source');
+     btnSeeLive.setAttribute('class', 'buttons pro-source');
+
+    const LiveLink = document.createElement('a');
+    LiveLink.setAttribute('href', project. button2);
+    LiveLink.appendChild(btnLive);
+    btnSeeLive.appendChild(LiveLink);
+  
+    divProject.appendChild(btnSeeLive);
+
+
   
     const htmlPage = document.getElementById('htmlPage');
     htmlPage.appendChild(divProject);
